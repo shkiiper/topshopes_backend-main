f416132a8958d1198ee78b532064c86bdf0807a1
Max <shygaev.xxx@gmail.com>
2023-03-01 00:14:09 +0600

    отмена add category to filtr

--- products/filters.py
+++ products/filters.py
@@ -7,8 +7,7 @@ class ProductFilter(FilterSet):
     max_price = django_filters.CharFilter(field_name="price", lookup_expr="lte")
     min_price = django_filters.CharFilter(field_name="price", lookup_expr="gte")
     brand = django_filters.CharFilter(field_name="brand__name", lookup_expr="in")
-    category = django_filters.CharFilter(field_name="category__name", lookup_expr="in")
 
     class Meta:
         model = Product
-        fields = ["max_price", "min_price", "brand", "category"]
+        fields = ["max_price", "min_price", "brand"]
