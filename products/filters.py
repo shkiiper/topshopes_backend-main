8d7bf0f5f8fbd09f0361ec688c20e34bd52564a2
Max <shygaev.xxx@gmail.com>
2023-03-01 11:08:00 +0600

    add category filters

--- products/filters.py
+++ products/filters.py
@@ -7,7 +7,8 @@ class ProductFilter(FilterSet):
     max_price = django_filters.CharFilter(field_name="price", lookup_expr="lte")
     min_price = django_filters.CharFilter(field_name="price", lookup_expr="gte")
     brand = django_filters.CharFilter(field_name="brand__name", lookup_expr="icontains")
+    category = django_filters.CharFilter(field_name="category__name", lookup_expr="icontains")
 
     class Meta:
         model = Product
-        fields = ["max_price", "min_price", "brand"]
+        fields = ["max_price", "min_price", "brand", "category"]
