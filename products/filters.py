01e01f900e4f1e5f98464c156a12622df09a6dd4
Max <shygaev.xxx@gmail.com>
2023-03-06 14:41:10 +0600

    test brand filter

--- products/filters.py
+++ products/filters.py
@@ -6,7 +6,7 @@ from .models import Product
 class ProductFilter(FilterSet):
     max_price = django_filters.CharFilter(field_name="price", lookup_expr="lte")
     min_price = django_filters.CharFilter(field_name="price", lookup_expr="gte")
-    brand = django_filters.MultipleChoiceFilter(field_name="brand__name", lookup_expr="icontains")
+    brand = django_filters.CharFilter(field_name="brand__name, brand__name", lookup_expr="icontains")
     category = django_filters.CharFilter(field_name="category__name", lookup_expr="icontains")
 
     class Meta:
