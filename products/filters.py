9d384d2cc5dd7bc715760461519f8f91c7dfdd8c
Max <shygaev.xxx@gmail.com>
2023-03-07 11:35:24 +0600

    return brand filter

--- products/filters.py
+++ products/filters.py
@@ -8,7 +8,7 @@ from .models import Product, Brand
 class ProductFilter(FilterSet):
     max_price = django_filters.CharFilter(field_name="price", lookup_expr="lte")
     min_price = django_filters.CharFilter(field_name="price", lookup_expr="gte")
-    brand = django_filters.CharFilter(field_name='brand__name', lookup_expr='icontains'), django_filters.CharFilter(field_name='brand__name', lookup_expr='icontains')
+    brand = django_filters.CharFilter(field_name='brand__name', lookup_expr='icontains')
     # brand = django_filters.filters.BaseCSVFilter(method='filter_brand')
     category = django_filters.CharFilter(field_name="category__name", lookup_expr="icontains")
 
