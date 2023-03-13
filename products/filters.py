e33549b1aca4d018cedb1cd353ae89441eab4940
shkiiper <shygaev.xxx@gmail.com>
2023-03-13 10:17:26 +0600

    df

--- products/filters.py
+++ products/filters.py
@@ -7,6 +7,7 @@ class ProductFilter(FilterSet):
     max_price = django_filters.CharFilter(field_name="price", lookup_expr="lte")
     min_price = django_filters.CharFilter(field_name="price", lookup_expr="gte")
     brand = django_filters.MultipleChoiceFilter(django_filters.CharFilter(field_name='brand__name', lookup_expr='icontains'))
+
     category = django_filters.CharFilter(field_name="category__name", lookup_expr="icontains")
 
     class Meta:
