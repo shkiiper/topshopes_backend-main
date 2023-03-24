4c8aa6f3a7b104f016dfaf2b24df041c78710f5f
shkiiper <shygaev.xxx@gmail.com>
2023-03-24 09:57:54 +0600

    create report3

--- report/routers.py
+++ report/routers.py
@@ -1,11 +1,6 @@
-from django.urls import path, include
 from rest_framework import routers
 from .views import PaidOrderList, CompletedOrderList
 
-router = routers.DefaultRouter()
+router = routers.SimpleRouter()
 router.register(r'orders/paid', PaidOrderList, basename='paid-orders')
 router.register(r'orders/completed', CompletedOrderList, basename='completed-orders')
-
-urlpatterns = [
-    path('', include(router.urls)),
-]
