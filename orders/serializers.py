750d8af1f43cd00b2789a7f87fb0a830afcb25f6
shkiiper <shygaev.xxx@gmail.com>
2023-03-24 15:53:56 +0600

    report in orders_13

--- orders/serializers.py
+++ orders/serializers.py
@@ -10,10 +10,6 @@ from django.conf import settings
 from redis.exceptions import LockError
 from products.models import ProductVariant, Product
 
-from rest_framework import serializers
-from products.serializers import CategorySerializer
-from .models import Order
-
 from .models import Order
 
 
@@ -112,21 +108,14 @@ class CreateOrderSerializer(serializers.ModelSerializer):
         return order
 
 
-class ProductSerializer(serializers.ModelSerializer):
-    category = CategorySerializer()
-
-    class Meta:
-        model = Product
-        fields = ['tax']
-
-
 class OrderTotalPriceSerializer(serializers.ModelSerializer):
-    tax = ProductSerializer()
+    product = ProductSerializer()
     profit = serializers.SerializerMethodField()
 
     class Meta:
         model = Order
-        fields = ["id", "created_at", 'total_price', 'tax', 'profit']
+        fields = ["id", "created_at", 'total_price', 'product', 'profit']
 
-    def get_profit(obj):
-        return obj.total_price - obj.tax.category.tax
+    def get_profit(self, obj):
+        return obj.total_price - obj.product.category.tax
+
