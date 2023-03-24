e922293034cc246bdafad6f1fec2b5e7ffce1a33
shkiiper <shygaev.xxx@gmail.com>
2023-03-24 13:57:48 +0600

    report in orders_3

--- orders/routers.py
+++ orders/routers.py
@@ -3,5 +3,5 @@ from rest_framework import routers
 from orders.views import OrderViewSet, OrderList
 
 router = routers.SimpleRouter()
-router.register(r"orders", OrderList, basename="orders")
+router.register(r"orders", OrderViewSet, basename="orders")
 router.register(r'orderList', OrderList, basename='orderList')
