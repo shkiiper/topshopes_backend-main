14c01c9aba8cc2bf9171c89bc29d635670efd59f
shkiiper <shygaev.xxx@gmail.com>
2023-03-24 16:21:45 +0600

    report in orders_19

--- orders/serializers.py
+++ orders/serializers.py
@@ -109,7 +109,7 @@ class CreateOrderSerializer(serializers.ModelSerializer):
 
 
 class OrderTotalPriceSerializer(serializers.ModelSerializer):
-    tax = serializers.DecimalField(max_digits=10, decimal_places=2)
+    tax = CategorySerializer.DecimalField(max_digits=10, decimal_places=2)
 
     class Meta:
         model = Order
