5148035e6a1c9ecbf0117b21abc8db7512e64995
shkiiper <shygaev.xxx@gmail.com>
2023-03-30 10:28:33 +0600

    is_published viewset products_7

--- products/views.py
+++ products/views.py
@@ -63,35 +63,33 @@ class ProductViewSet(
     ordering_fields = ["name", "rating", "overall_price", "created_at", "discount", "price"]
 
     def get_queryset(self):
-        if Product.objects.filter(is_published=True).prefetch_related('variants'):
-            if self.action == "list":
-                return (
-                    Product.objects.prefetch_related("variants")
-                    .all()
-                    .annotate(
-                        overall_price=Subquery(
-                            ProductVariant.objects.filter(product=OuterRef("pk")).values(
-                                "overall_price"
-                            )[:1]
-                        ),
-                        discount_price=Subquery(
-                            ProductVariant.objects.filter(product=OuterRef("pk")).values(
-                                "discount_price"
-                            )[:1]
-                        ),
-                        price=Subquery(
-                            ProductVariant.objects.filter(product=OuterRef("pk")).values(
-                                "price"
-                            )[:1]
-                        ),
-                        discount=Subquery(
-                            ProductVariant.objects.filter(product=OuterRef("pk")).values(
-                                "discount"
-                            )[:1]
-                        ),
-
-                    )
+        if self.action == "list":
+            return (
+                Product.objects.prefetch_related("variants")
+                .filter(is_published=True)  # filter only published products
+                .annotate(
+                    overall_price=Subquery(
+                        ProductVariant.objects.filter(product=OuterRef("pk")).values(
+                            "overall_price"
+                        )[:1]
+                    ),
+                    discount_price=Subquery(
+                        ProductVariant.objects.filter(product=OuterRef("pk")).values(
+                            "discount_price"
+                        )[:1]
+                    ),
+                    price=Subquery(
+                        ProductVariant.objects.filter(product=OuterRef("pk")).values(
+                            "price"
+                        )[:1]
+                    ),
+                    discount=Subquery(
+                        ProductVariant.objects.filter(product=OuterRef("pk")).values(
+                            "discount"
+                        )[:1]
+                    ),
                 )
+            )
         return Product.objects.all().prefetch_related("variants", "reviews")
 
     def get_serializer_class(self):
@@ -224,38 +222,37 @@ class ShopProductViewSet(viewsets.ModelViewSet):
         """
         Returns only current user's shop products
         """
-        if Product.objects.filter(is_published=True).prefetch_related('variants'):
-            return (
-                Product.objects.prefetch_related("variants")
-                .filter(shop=self.request.user.shop)  # type: ignore
-                .annotate(
-                    overall_price=Subquery(
-                        ProductVariant.objects.filter(product=OuterRef("pk")).values(
-                            "overall_price"
-                        )[:1]
-                    ),
-                    discount_price=Subquery(
-                        ProductVariant.objects.filter(product=OuterRef("pk")).values(
-                            "discount_price"
-                        )[:1]
-                    ),
+        return (
+            Product.objects.prefetch_related("variants")
+            .filter(shop=self.request.user.shop, is_published=True)  # filter only published products
+            .annotate(
+                overall_price=Subquery(
+                    ProductVariant.objects.filter(product=OuterRef("pk")).values(
+                        "overall_price"
+                    )[:1]
+                ),
+                discount_price=Subquery(
+                    ProductVariant.objects.filter(product=OuterRef("pk")).values(
+                        "discount_price"
+                    )[:1]
+                ),
 
-                    price=Subquery(
-                        ProductVariant.objects.filter(product=OuterRef("pk")).values(
-                            "price"
-                        )[:1]
-                    ),
-                    discount=Subquery(
-                        ProductVariant.objects.filter(product=OuterRef("pk")).values(
-                            "discount"
-                        )[:1]
-                    ),
-                    thumbnail=Subquery(
-                        ProductVariant.objects.filter(product=OuterRef("pk")).values(
-                            "thumbnail"
-                        )[:1]
-                    ),
-                )
+                price=Subquery(
+                    ProductVariant.objects.filter(product=OuterRef("pk")).values(
+                        "price"
+                    )[:1]
+                ),
+                discount=Subquery(
+                    ProductVariant.objects.filter(product=OuterRef("pk")).values(
+                        "discount"
+                    )[:1]
+                ),
+                thumbnail=Subquery(
+                    ProductVariant.objects.filter(product=OuterRef("pk")).values(
+                        "thumbnail"
+                    )[:1]
+                ),
+            )
         )
 
     def update(self, request, *args, **kwargs):
