dcad104c320bb2d8d9ac039e4682983aeda2d9af
shkiiper <shygaev.xxx@gmail.com>
2023-04-03 11:04:17 +0600

    payment filter

--- payments/urls.py
+++ payments/urls.py
@@ -4,5 +4,5 @@ from .views import ReportAdmin, ReportClient, PaymentFilter
 urlpatterns = [
     path("report-admin/", ReportAdmin.as_view()),
     path("report-client/", ReportClient.as_view()),
-    path("PaymentFilter/", PaymentFilter.as_view()),
+    path("payment-filter/", PaymentFilter.as_view()),
 ]
