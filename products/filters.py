b8d4eee8d24fe9b0f898e588e7d0d826bfe8fcbc
shkiiper <shygaev.xxx@gmail.com>
2023-04-12 10:14:13 +0600

    filters

--- products/filters.py
+++ products/filters.py
@@ -1,6 +1,10 @@
 import django_filters
 from django.utils import timezone
-from .models import Product, Brand
+from django.db.models import OuterRef, Subquery
+from .models import Product, Brand, ProductVariant
+from django.db.models import F
+
+from .serializers import ProductVariantSerializer
 
 
 class ProductFilter(django_filters.FilterSet):
@@ -16,16 +20,31 @@ class ProductFilter(django_filters.FilterSet):
     new_arrival = django_filters.BooleanFilter(method='filter_new_arrival')
     min_rating = django_filters.NumberFilter(field_name="rating", lookup_expr='gte')
     max_rating = django_filters.NumberFilter(field_name="rating", lookup_expr='lte')
+    serializer_class = ProductVariantSerializer
 
     def filter_has_discount(self, queryset, name, value):
-        if value:
-            return queryset.filter(variants__discount_price__isnull=False)
+        queryset = (
+            Product.objects.filter(variants__discount__gt=0, is_published=True)
+            .annotate(
+                discounted_price=F("variants__price") - (F("variants__price") * F("variants__discount") / 100),
+                price_annotation=F("variants__price"),
+            )
+            .order_by("-discounted_price")
+        )
         return queryset
 
     def filter_new_arrival(self, queryset, name, value):
-        if value:
-            yesterday = timezone.now() - timezone.timedelta(days=2)
-            queryset = queryset.filter(created_at__gte=yesterday)
+        queryset = (
+            Product.objects.all()
+            .prefetch_related("variants")
+            .annotate(
+                price=Subquery(
+                    ProductVariant.objects.filter(product=OuterRef("pk")).values(
+                        "price"
+                    )[:1]
+                )
+            )
+        )
         return queryset
 
     class Meta:
