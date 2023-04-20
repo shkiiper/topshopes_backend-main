66591e463dfc946fc5ffd74fa93a72e253bad7d6
shkiiper <shygaev.xxx@gmail.com>
2023-04-20 11:18:15 +0600

    update SingleShopSerializer

--- attributes/views.py
+++ attributes/views.py
@@ -2,31 +2,10 @@ from rest_framework import status
 from rest_framework import mixins, permissions, viewsets
 from rest_framework.response import Response
 from core.permissions import HasShop
+from .models import AttributeValue
+from .serializers import AttributeValueSerializer
 
-from .models import AttributeValue, Attribute
-from .serializers import AttributeValueSerializer, AttributeSerializer
 
-
-# class AttributeValueViewset(
-#     mixins.UpdateModelMixin, mixins.DestroyModelMixin, viewsets.GenericViewSet
-# ):
-#     """
-#     Viewset to delete and update product attributes
-#     Awailable only for sellers
-#     """
-#
-#     queryset = AttributeValue.objects.all()
-#     serializer_class = AttributeValueSerializer
-#     permission_classes = [permissions.IsAuthenticated, HasShop]
-#
-#     def update(self, request, *args, **kwargs):
-#         instance = self.get_object()
-#         serializer = self.get_serializer(instance, data=request.data, partial=True)
-#         serializer.is_valid(raise_exception=True)
-#         self.perform_update(serializer)
-#         # удаляем все значения, связанные с атрибутом
-#         instance.values.all().delete()
-#         return Response(serializer.data)
 class AttributeValueViewset(
     mixins.UpdateModelMixin, mixins.DestroyModelMixin, viewsets.GenericViewSet
 ):
