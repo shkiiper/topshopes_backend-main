70a88d659d9bc6458de2fc1ba91d44501fb9761e
shkiiper <shygaev.xxx@gmail.com>
2023-05-11 17:58:55 +0600

    add quantity field for Products

--- products/models.py
+++ products/models.py
@@ -192,18 +192,21 @@ class ProductVariant(models.Model):
         return self.product.name
 
     def save(self, *args, **kwargs):
+
         if self.discount:
             self.discount_price = self.price - (self.price * Decimal(self.discount) / Decimal(100))
         else:
             self.discount_price = 0
+            self.overall_price = self.price - (self.price * self.product.category.tax / 100)
 
         self.overall_price = self.discount_price - (self.discount_price * self.product.category.tax / 100)
-        if self.discount_price == 0:
-            self.overall_price = self.price - (self.price * self.product.category.tax/100)
 
         if self.product.shop.status == "special":
             self.product.category.tax = 10
             self.overall_price = self.discount_price - (self.discount_price * self.product.category.tax / 100)
+
+        # if self.discount_price == 0:
+        #     self.overall_price = self.price - (self.price * self.product.category.tax/100)
 
         self.tax_price = self.discount_price * self.product.category.tax / 100
 
